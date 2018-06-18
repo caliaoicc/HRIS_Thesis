441f1075a9ac8478bd421fd5b19e50b3fc22e294
ICC <34598786+caliaoicc@users.noreply.github.com>
2018-06-19 04:37:45 +0800

    Add files via upload
    Added login module but does not contain error messages

---
+++ loginForm/includes/dbhandler.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "sampleloginsystem";
+
+$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
